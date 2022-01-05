cd4bde20285b9acb92039f8d5b94a9c4b3465eac
Nattan Gama <91083215+NattanGama@users.noreply.github.com>
2022-01-04 23:38:04 -0300

    Create Pessoa.cs

---
+++ Abstração/Pessoa.cs
@@ -0,0 +1,17 @@
+usando o sistema;
+
+namespace ExemploPOO. Modelos
+{
+     classe pública Pessoa
+    {
+         cadeia pública Nome {get; pôr;} 
+
+        int  Idade pública {obter; pôr;}
+
+         vazio púb Apresentarlico () {
+
+            Console. WriteLine ($"Olá, meu nome é {Nome} e tenho {Idade}anos. "  );
+
+        }
+    }
+}
